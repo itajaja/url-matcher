eff95afdda823f12a08f15d2bdc9faa2c35073cf
Giacomo Tagliabue <giacomo.tag@gmail.com>
2015-11-05 14:21:35 +0100

    eslint `prefer-arrow-callback`

--- modules/__tests__/index-test.js
+++ modules/__tests__/index-test.js
@@ -1,7 +1,7 @@
 import expect from 'expect'
 import { matchPattern } from '../matcher'
 
-describe('matchPattern', function () {
+describe('matchPattern', () => {
 
   function assertMatch(pattern, pathname, remainingPathname, paramNames, paramValues) {
     expect(matchPattern(pattern, pathname)).toEqual({
@@ -11,33 +11,33 @@ describe('matchPattern', function () {
     })
   }
 
-  it('works without params', function () {
+  it('works without params', () => {
     assertMatch('/', '/path', 'path', [], [])
   })
 
-  it('works with named params', function () {
+  it('works with named params', () => {
     assertMatch('/:id', '/path', '', [ 'id' ], [ 'path' ])
     assertMatch('/:id.:ext', '/path.jpg', '', [ 'id', 'ext' ], [ 'path', 'jpg' ])
   })
 
-  it('works with named params that contain spaces', function () {
+  it('works with named params that contain spaces', () => {
     assertMatch('/:id', '/path+more', '', [ 'id' ], [ 'path+more' ])
     assertMatch('/:id', '/path%20more', '', [ 'id' ], [ 'path more' ])
   })
 
-  it('works with splat params', function () {
+  it('works with splat params', () => {
     assertMatch('/files/*.*', '/files/path.jpg', '', [ 'splat', 'splat' ], [ 'path', 'jpg' ])
   })
 
-  it('ignores trailing slashes', function () {
+  it('ignores trailing slashes', () => {
     assertMatch('/:id', '/path/', '', [ 'id' ], [ 'path' ])
   })
 
-  it('works with greedy splat (**)', function () {
+  it('works with greedy splat (**)', () => {
     assertMatch('/**/g', '/greedy/is/good/g', '', [ 'splat' ], [ 'greedy/is/good' ])
   })
 
-  it('works with greedy and non-greedy splat', function () {
+  it('works with greedy and non-greedy splat', () => {
     assertMatch('/**/*.jpg', '/files/path/to/file.jpg', '', [ 'splat', 'splat' ], [ 'files/path/to', 'file' ])
   })
 
