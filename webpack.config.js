bc1f1c63078a5ea087533e2c926e7ac2e92b766a
Giacomo Tagliabue <giacomo.tag@gmail.com>
2015-11-05 19:48:57 +0100

    minify build

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,9 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false }
     })
   ]
 
