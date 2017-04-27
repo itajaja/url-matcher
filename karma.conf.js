e3df0fa74b3b439e7863a74d33400afa3ac2c1de
Giacomo Tagliabue <giacomo.tag@gmail.com>
2017-04-27 16:58:43 -0400

    revamp build

--- karma.conf.js
+++ karma.conf.js
@@ -1,35 +1,11 @@
-var webpack = require('webpack')
 var path = require('path')
 
 module.exports = function (config) {
 
   config.set({
-
     browsers: [ 'Chrome' ],
     frameworks: [ 'mocha' ],
     reporters: [ 'mocha' ],
-
-    files: [
-      'tests.webpack.js'
-    ],
-
-    preprocessors: {
-      'tests.webpack.js': [ 'webpack', 'sourcemap' ]
-    },
-
-    webpack: {
-      devtool: 'inline-source-map',
-      module: {
-        loaders: [
-          { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
-        ]
-      },
-      plugins: [
-        new webpack.DefinePlugin({
-          'process.env.NODE_ENV': JSON.stringify('test')
-        })
-      ]
-    }
   })
 
 }
